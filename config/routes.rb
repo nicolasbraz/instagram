c2c878e497fed334eb72ac359e98172f457d397e
Nicolas <nicolas.braz@gmail.com>
2022-12-04 23:13:11 -0300

    Feat: Sign_in as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'home#index'
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
