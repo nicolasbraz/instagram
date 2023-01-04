aa58a96123461d128a78d32735534498294da2ed
Nicolas <nicolas.braz@gmail.com>
2023-01-03 22:03:57 -0300

    Feat: Adding options to the filepond.create

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,5 +1,13 @@
 import * as FilePond from 'filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'; 
+
+FilePond.registerPlugin(FilePondPluginImagePreview);
 
 const inputElement = document.querySelector('#post-images');
 
-const pond = FilePond.create(inputElement);
+const pond = FilePond.create(inputElement, {
+  credits: {},
+  storeAsFile: true,
+  allowMultiple: true,
+  allowReorder: true
+});
