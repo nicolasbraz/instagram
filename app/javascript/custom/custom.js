d4d72973bf07838b434589af9fd19d693bb89e3c
Nicolas <nicolas.braz@gmail.com>
2023-01-06 23:24:18 -0300

    Feat: Adding only images in the post

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,7 +1,11 @@
 import * as FilePond from 'filepond';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'; 
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
-FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateType
+);
 
 const inputElement = document.querySelector('#post-images');
 
@@ -9,5 +13,6 @@ const pond = FilePond.create(inputElement, {
   credits: {},
   storeAsFile: true,
   allowMultiple: true,
-  allowReorder: true
+  allowReorder: true,
+  acceptedFileTypes: ['image/*']
 });
