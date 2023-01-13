7d4b24f17df928d47a212a62b1b3086b18c1534a
Nicolas <nicolas.braz@gmail.com>
2023-01-13 20:28:23 -0300

    Feat: Finishing the use of loadFilePond

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -2,17 +2,21 @@ import * as FilePond from 'filepond';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'; 
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginFileValidateType
-);
+document.addEventListener("turbo:load", loadFilePond);
 
-const inputElement = document.querySelector('#post-images');
+function loadFilePond(){
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType
+  );
 
-const pond = FilePond.create(inputElement, {
-  credits: {},
-  storeAsFile: true,
-  allowMultiple: true,
-  allowReorder: true,
-  acceptedFileTypes: ['image/*']
-});
+  const inputElement = document.querySelector('#post-images');
+
+  const pond = FilePond.create(inputElement, {
+    credits: {},
+    storeAsFile: true,
+    allowMultiple: true,
+    allowReorder: true,
+    acceptedFileTypes: ['image/*']
+  });
+}
