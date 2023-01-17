90807175afc20dde9ecea94bec86e5f8faf6f6ce
Nicolas <nicolas.braz@gmail.com>
2023-01-16 23:02:18 -0300

    Feat: Adding the toggle_like route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root 'home#index'
+  post 'toggle_like', to: 'likes#toggle_like', as: :toggle_like
   # devise_scope :user do
   #   root 'devise/sessions#new'
   # end
