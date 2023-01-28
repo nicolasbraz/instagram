2bb730b3438e5027f8e9fdfa74332b94e69397a6
Nicolas <nicolas.braz@gmail.com>
2023-01-27 22:20:26 -0300

    Feat: Creating the set_post and before_action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
-  before_action :set_post
+  before_action :set_post, only: [:create]
 
   def create
-    @post.comments.create(user: current_user, body: params[:comment_body])
+    @comment = @post.comments.create(user: current_user, body: params[:comment_body])
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if (@comment.user = current_user)
+      @comment.destroy
+    end
   end
 
   private
